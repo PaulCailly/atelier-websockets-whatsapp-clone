bb9f1077265ba0e988596f89c6533de6011b3d05
Paul Cailly <hello@paulcailly.fr>
2019-12-21 21:18:45 +0100

    :construction: wip

---
+++ client/build/precache-manifest.a6ff87d64388e3839b397a5b20d73175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2320a85e003cd59f81163300e66297ec",
+    "url": "/websockets-whatsapp-clone/index.html"
+  },
+  {
+    "revision": "b826beb7fd2cd4790a85",
+    "url": "/websockets-whatsapp-clone/static/css/main.33adcb45.chunk.css"
+  },
+  {
+    "revision": "7daf20959679aabc3170",
+    "url": "/websockets-whatsapp-clone/static/js/2.eb3adf63.chunk.js"
+  },
+  {
+    "revision": "b826beb7fd2cd4790a85",
+    "url": "/websockets-whatsapp-clone/static/js/main.2509f860.chunk.js"
+  },
+  {
+    "revision": "b530e0e0c774114fc0c8",
+    "url": "/websockets-whatsapp-clone/static/js/runtime-main.9de6cc61.js"
+  }
+]);
