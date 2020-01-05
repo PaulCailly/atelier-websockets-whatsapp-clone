501be3b464eea8ab82cc23790eafbfb3038789be
Paul Cailly <hello@paulcailly.fr>
2020-01-05 19:12:30 +0100

    start

---
+++ client/src/components/message-input/MessageInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+const MessageInput = ({ handleSubmit, input }) => {
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          placeholder="Votre message"
+          value={input.value}
+          onChange={input.onChange}
+          autoFocus
+        />
+        <button className="hidden" />
+      </form>
+    </div>
+  );
+};
+
+MessageInput.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  input: PropTypes.shape({
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired
+  })
+};
+
+export default MessageInput;
