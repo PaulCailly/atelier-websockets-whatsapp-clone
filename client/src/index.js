fa0f362b286591b2b1e632bc287f4de5e0e33733
Paul Cailly <hello@paulcailly.fr>
2020-01-05 18:00:01 +0100

    01 - introduction

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+// Modules
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
